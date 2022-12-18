0a22dfeeedc2f447d0fa826733588585048f242d
Marcin <lmartino@wp.pl>
2022-12-18 04:20:55 +0100

    nowe

---
+++ Kod/14. Zmienne/script.js
@@ -0,0 +1,42 @@
+let numericVal = -5;
+let stringVal = "Wartość";
+let booleanVal = true;
+
+console.log(numericVal);
+console.log(stringVal);
+console.log(booleanVal);
+
+console.log('---------------');
+
+console.log(typeof numericVal);
+console.log(typeof stringVal);
+console.log(typeof booleanVal);
+
+console.log('---------------');
+
+numericVal = "Nowa wartosc";
+console.log(numericVal);
+console.log(typeof numericVal)
+
+console.log('---------------');
+
+let sum = '1' + 1;
+console.log(sum);
+
+let diff = '1' - 1;
+console.log(diff);
+
+let sum1 = 1 + '1';
+console.log(sum1);
+
+let valueFormUser = '1';
+let sum2 = 1 + parseInt(valueFormUser);
+console.log(sum);
+
+console.log('---------------');
+
+
+
+
+
+
